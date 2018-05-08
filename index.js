d0e94231f381539d4ec2d70dac848be9251f697f
guymarsden01@gmail.com <guymarsden01@gmail.com>
2018-05-08 08:25:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   
-  for (var i=0; i<=cart.length;i++) {
+  for (var i=0; i<cart.length;i++) {
   	if (cart[i].itemName==item) {
   		cart.splice(i,1)
 	  	return cart }
