813cb9e8b92867024c4eca2362bb06c463ac7755
guymarsden01@gmail.com <guymarsden01@gmail.com>
2018-05-08 08:33:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,10 +81,10 @@ function placeOrder(cardNumber) {
   }
   
   else {
-    var cost = total
+    var cost = total()
     for (var i=0; i<cart.length;i++) {
       removeFromCart(cart[i].itemName)
     }
-  return (`Your total cost is $${total}, which will be charged to the card ${cardNumber}`)
+  return (`Your total cost is $${cost}, which will be charged to the card ${cardNumber}.`)
   }
 }
